1d53f0aa1196971ba7c58903925ebd8288224575
Ashoktalabattula <ashoktalabattula94@gmail.com>
2024-04-26 19:47:12 +0530

    few changes done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sweyawebsitepackages';
+
+  routingdata(event:any){
+
+    console.log(event);
+
+  }
+
+  footerroutingdata(event: any){
+    console.log(event)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BacktotopbuttonModule } from '../../projects/backtotopbutton/src/public-api';
+import { HeaderModule } from '../../projects/header/src/public-api';
+import { FooterModule } from '../../projects/footer/src/public-api';
+import { SitecontactformModule } from '../../projects/sitecontactform/src/public-api';
+import { SitejobapplicationModule } from '../../projects/sitejobapplication/src/public-api';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BacktotopbuttonModule,
+    HeaderModule,
+    FooterModule,
+    SitecontactformModule,
+    SitejobapplicationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
